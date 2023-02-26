45ed81e0087504b3abb6452400f152dcabbf9fdc
jon-cron <jonathanacm95@gmail.com>
2023-02-26 07:54:41 -0700

    push

---
+++ src/components/Title.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Title = () => {
+
+  return (
+    <div>
+      <h1>New Title</h1>
+      <br/>
+      <h2>more words</h2>
+    </div>
+  )
+}
+
+export default Title
+
+// export default function Title(){
+//   return
+// }

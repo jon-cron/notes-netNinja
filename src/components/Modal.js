5e0848bbf6659b5773c46cccc68d80fa479147a3
jon-cron <jonathanacm95@gmail.com>
2023-02-26 09:04:35 -0700

    portal working

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import { createPortal } from "react-dom"
 import './Modal.css'
+// NOTE this modal works almost identically with or without createPortal. createPortal just hides the html when the modal is not shown
 const Modal = ({children, handleClose}) => {
-  return (
+  return ( createPortal((
+
     <div className="modal-backdrop">
       <div className="modal">
         {children}
         <button onClick={handleClose}>Close</button>
       </div>
     </div>
+      ), document.body)
   )
 }
 

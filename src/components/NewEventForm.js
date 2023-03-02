fa7189cd0f1a763bb8abef3d7d7d30f4ca0a90a4
jon-cron <jonathanacm95@gmail.com>
2023-03-02 11:12:11 -0700

    back to useState

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,44 +1,43 @@
 import "./NewEventForm.css";
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
 
 const NewEventForm = ({ addEvent }) => {
-  const title = useRef();
-  const date = useRef();
+  // const title = useRef();
+  // const date = useRef();
 
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const event = {
+  //     title: title.current.value,
+  //     date: date.current.value,
+  //     id: Math.floor(Math.random() * 10000),
+  //   };
+  //   console.log(event);
+  //   addEvent(event);
+  //   resetForm();
+  // };
+  // const resetForm = () => {
+  //   title.current.value = "";
+  //   date.current.value = "";
+  // };
+
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
     const event = {
-      title: title.current.value,
-      date: date.current.value,
+      title: title,
+      date: date,
       id: Math.floor(Math.random() * 10000),
     };
-    console.log(event);
+    // NOTE passing the object up to the parent through props
     addEvent(event);
     resetForm();
   };
   const resetForm = () => {
-    title.current.value = "";
-    date.current.value = "";
+    setTitle("");
+    setDate("");
   };
-
-  // const [title, setTitle] = useState('')
-  // const [date, setDate] = useState('')
-  // NOTE old submit uses useState()
-  // const handleSubmit = (e) => {
-  //   e.preventDefault()
-  //   const event = {
-  //     title: title,
-  //     date: date,
-  //     id: Math.floor(Math.random() * 10000)
-  //   }
-  //   // NOTE passing the object up to the parent through props
-  //   addEvent(event)
-  //   resetForm()
-  // }
-  // const resetForm = () => {
-  //   setTitle('')
-  //   setDate('')
-  // }
   return (
     <form className="new-event-form">
       {/* NOTE these are both acceptable ways of labeling a form input */}
@@ -53,16 +52,18 @@ const NewEventForm = ({ addEvent }) => {
         <span>Event Title:</span>
         <input
           type="text"
-          ref={title}
-          // onChange={(e) => setTitle(e.target.value)}
+          // ref={title}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
       </label>
       <label>
         <span>Event Date:</span>
         <input
-          ref={date}
+          // ref={date}
+          value={date}
           type="date"
-          // onChange={(e) => setDate(e.target.value)}
+          onChange={(e) => setDate(e.target.value)}
         />
       </label>
       <button onClick={(e) => handleSubmit(e)}>Submit</button>
